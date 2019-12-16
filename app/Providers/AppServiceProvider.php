5ae6afd28a78604373777327835ddbfbabfac484
Nabeel Yousaf Pasha <nabeelyousafpasha@gmail.com>
2019-12-16 13:12:40 +0500

    Initial Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class   AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
